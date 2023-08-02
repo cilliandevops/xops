040540366ac774e8c954c013b38e7cd8f83b0465
cilliandevops <cilliandevops@gmail.com>
2023-08-02 15:55:06 +0800

    feat
    CreateFrontend

---
+++ xops-fe/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+const pathSrc = path.resolve(__dirname, "src");
+
+// 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver()
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver()
+      ],
+    }),
+  
+  ],
+  resolve: {
+    alias: {
+        "@": pathSrc
+    }
+}
+})
